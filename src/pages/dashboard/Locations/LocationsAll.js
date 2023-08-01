f59a618d8fd8c1e4e20bd5b57218f4559399f5a9
Kez Anwar <kezanwar@gmail.com>
2023-08-01 07:12:02 +0100

    added delete locations functonality to locations dashboard

--- src/pages/dashboard/Locations/LocationsAll.js
+++ src/pages/dashboard/Locations/LocationsAll.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useCallback, useMemo, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet-async';
+import { useSnackbar } from 'notistack';
 import { Box, Container, Typography, styled } from '@mui/material';
 import { useNavigate } from 'react-router';
 import DriveFileRenameOutlineOutlinedIcon from '@mui/icons-material/DriveFileRenameOutlineOutlined';
@@ -9,11 +10,15 @@ import { DashboardTitleContainer } from '../styles';
 import DashboardTitle from '../../../components/dashboard-title/DashboardTitle';
 import LoadingScreen from '../../../components/loading-screen/LoadingScreen';
 import LightLoadingButton from '../../../components/light-loading-button/LightLoadingButton';
-
-import useRestaurantQuery from '../../../hooks/queries/useRestaurantQuery';
 import LocationCard from '../../../components/location-card/LocationCard';
+import AcceptDeclineModal from '../../../components/accept-decline-modal/AcceptDeclineModal';
+
 import useLocationsQuery from '../../../hooks/queries/useLocationsQuery';
+import useRestaurantQuery from '../../../hooks/queries/useRestaurantQuery';
+
 import { PATH_DASHBOARD } from '../../../routes/paths';
+import { deleteLocation } from '../../../utils/api';
+import { MIXPANEL_EVENTS, mixpanelTrack } from '../../../utils/mixpanel';
 
 export const LocationsWrapper = styled(Box)(({ theme }) => ({
   display: 'flex',
@@ -23,6 +28,13 @@ export const LocationsWrapper = styled(Box)(({ theme }) => ({
 }));
 
 const LocationsAll = (props) => {
+  const [deleteLocationLoading, setDeleteLocationLoading] = useState(false);
+  const [deleteLocationModalOpen, setDeleteLocationModalOpen] = useState(false);
+
+  const { enqueueSnackbar } = useSnackbar();
+
+  const idToDelete = useRef(null);
+
   const resQuery = useRestaurantQuery();
   const locQuery = useLocationsQuery();
   const nav = useNavigate();
@@ -34,6 +46,45 @@ const LocationsAll = (props) => {
   const restLoading = resQuery?.isLoading;
 
   const onAddLocationClick = () => nav(PATH_DASHBOARD.locations_add);
+
+  const onCancelDeleteLocationModal = () => {
+    setDeleteLocationModalOpen(false);
+  };
+
+  const onDeleteLocationClick = (_id) => {
+    idToDelete.current = _id;
+    setDeleteLocationModalOpen(true);
+  };
+
+  const onConfirmLocationDelete = useCallback(async () => {
+    try {
+      setDeleteLocationLoading(true);
+      const res = await deleteLocation(idToDelete.current);
+      const data = res?.data;
+      locQuery.updateQuery(data);
+      enqueueSnackbar('Location deleted successfully');
+      mixpanelTrack(MIXPANEL_EVENTS.delete_location_success);
+      setDeleteLocationLoading(false);
+      setDeleteLocationModalOpen(false);
+    } catch (error) {
+      enqueueSnackbar(`${error?.message || 'Unexpected error occured'}`, {
+        variant: 'error'
+      });
+      setDeleteLocationLoading(false);
+      setDeleteLocationModalOpen(false);
+      mixpanelTrack(MIXPANEL_EVENTS.delete_location_failed, {
+        error: error?.message
+      });
+    }
+  }, []);
+
+  const deleteModalText = useMemo(() => {
+    if (idToDelete.current) {
+      const l = locations.find((lts) => lts._id === idToDelete.current);
+      return `Are you sure you want to delete ${l?.nickname}, ${l?.address?.postcode}?`;
+    }
+    return '';
+  }, [idToDelete.current]);
 
   if (restLoading) return <LoadingScreen />;
   return (
@@ -64,13 +115,23 @@ const LocationsAll = (props) => {
                       {...location}
                       key={location._id}
                       //   onEdit={onEditLocationClick}
-                      //   onDelete={onDeleteLocationClick}
+                      onDelete={onDeleteLocationClick}
                     />
                   );
                 })
                 .reverse()
             : null}
         </LocationsWrapper>
+        {deleteLocationModalOpen && (
+          <AcceptDeclineModal
+            title={'Confirm delete location'}
+            subtitle={deleteModalText}
+            isOpen={deleteLocationModalOpen}
+            submitLoading={deleteLocationLoading}
+            onCancel={onCancelDeleteLocationModal}
+            onAccept={onConfirmLocationDelete}
+          />
+        )}
       </Container>
     </>
   );
